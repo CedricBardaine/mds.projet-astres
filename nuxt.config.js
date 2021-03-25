096d1373f1dcd154ece32680092b268d8106a108
cedrix <cedricbardaine@gmail.com>
2021-03-26 00:13:32 +0100

    dl API data & implement content module for Nuxt & call content in index

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxt/content',
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <v-container> </v-container>
+  <v-container>
+    
+  </v-container>
 </template>
 
 <script>
 export default {
   components: {},
+  mounted() {
+    this.$content('local_api_data').fetch().then((rep) => {
+      let ret =""; 
+      rep.bodies.forEach(astralBody => {
+        ret += astralBody.id + " | "; 
+      });
+        console.log(ret);
+    })  
+},
 };
 </script>
 
